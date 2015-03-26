fba71655ee4acdb441b578a01645919cd47f3b56
somkiat <somkiat.p@gmail.com>
2015-03-26 19:06:21 +0700

    Update demo

--- app/src/androidTest/java/com/example/pages/StartPage.java
+++ app/src/androidTest/java/com/example/pages/StartPage.java
@@ -4,7 +4,6 @@ import com.example.somkiat.calculate.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
